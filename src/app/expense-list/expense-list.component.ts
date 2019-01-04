8f0ba7642d7a0bfe060df3a279177c333f3c0be0
fervil88 <fernandovillar88@gmail.com>
2019-01-03 23:37:38 -0300

    Added paginator but not completed

--- src/app/expense-list/expense-list.component.ts
+++ src/app/expense-list/expense-list.component.ts
@@ -1,25 +1,37 @@
 import { ExpenseService } from '../shared/expense/expense.service';
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { ExpenseModule } from '../shared/expense/expense.module';
 
 @Component({
     selector: 'app-expense-list',
     templateUrl: './expense-list.component.html',
     styleUrls: ['./expense-list.component.css']
 })
-export class ExpenseListComponent implements OnInit {
-    expenses: Array<any>;
+export class ExpenseListComponent implements OnInit, AfterViewInit  {
+
+    dataSource: MatTableDataSource<ExpenseModule>;
     isAuthenticated: boolean;
     displayedColumns: string[] = ['title', 'quotaStart', 'quotaEnd', 'amount', 'individual', 'owner', 'creationDateTime', 'actions'];
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+
 
     constructor(private router: Router,
-        private expenseService: ExpenseService) { }
+        private expenseService: ExpenseService) {
+            const expenses: ExpenseModule[] = [];
+            this.dataSource = new MatTableDataSource(expenses);
+         }
 
     async ngOnInit() {
         this.expenseService.getAll().subscribe(data => {
-            this.expenses = data;
+            this.dataSource = data;
         });
     }
+
+    ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
     gotoList() {
         this.router.navigateByUrl('/expense-list', {skipLocationChange: true}).then(() =>
@@ -28,8 +40,8 @@ export class ExpenseListComponent implements OnInit {
 
     delete(id: String, i: number) {
         this.expenseService.delete(id).subscribe(() => {
-            this.expenses.splice(i, 1);
-            this.expenses = JSON.parse(JSON.stringify(this.expenses));
+            this.dataSource.data.splice(i, 1);
+            this.dataSource = JSON.parse(JSON.stringify(this.dataSource));
         }, error => console.error(error));
     }
 
